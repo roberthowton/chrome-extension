1ce906198a46b6264758fd682fe043e71065e2dc
Robert Howton <robert.f.howton@gmail.com>
2021-10-13 18:10:50 -0400

    still following tutorial
    Co-authored-by: John Bryan <johnnybryan@users.noreply.github.com>

---
+++ extension.js
@@ -0,0 +1,9 @@
+// extension.js
+// add extension code here
+
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
